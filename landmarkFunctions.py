d7192cc012242f80c9ed4a15ee0ffb6b333014a4
Daniel Neenan <daniel@automise.co.uk>
2018-01-27 19:47:05 +0000

    Landmark update

--- landmarkFunctions.py
+++ landmarkFunctions.py
@@ -2,59 +2,59 @@ import numpy as np
 import time
 
 # calculate the effect landmarks have on player
-class landmark(object):
-    landmarkBaseMod = 0.5
 
-    def __int__(self, name, landmarkResources, landmarkItems, landmarkTeam):
-        self.name = name
-        self.landmarkResources = landmarkResources
-        self.landmarkItems = []
-        self.landmarkTeam = landmarkTeam
-
-    def addItem(self, item):
-        self.landmarkItems.append(item)
-
-    def captureLandmark(self, resources)
-        self.landmarkResources = resources
-
-landmarkMod = landmarkBaseMod * itemContagion
+#landmarkMod = landmarkBaseMod * itemContagion
+#landmarks need to be changed to the array of nearby landmarks in range
 def landmarkPressureInfluence(landmarkMod, effectiveMod, landmarks):
-    if landmarks in dist:
-        if "landmarks->team" == "players->team":
+    if landmark in landmarks:
+        #below logic needs to be changed
+        if landmarks == blue:
             for landmark in landmarks:
-                return effectiveMod = effectiveMod + landmarkMod
-        if "landmarks->team" != "players->team"
+                effectiveMod = 1 + effectiveMod + landmarkMod
+                return effectiveMod
+        #below logic needs to be changed
+        if landmarks != blue:
             for landmark in landmarks:
                 effectiveMod = effectiveMod - landmarkMod
                 if effectiveMod > -0.75:
+                    effectiveMod = 1 - effective
                     return effectiveMod
-                else:
-                    return effectiveMod = -0.75
+                elif effectiveMod <= -0.75:
+                    effectiveMod = 1 - 0.75
+                    return effectiveMod
 
 # landmark growth rate
-def landmarkResourceRate(itemResourceRate, itemImmuity):
-    decayRate = (300 + itemResourceRate) * itemImmunity
+def landmarkResourceRate(itemResourceRate, itemImmunity, landmarkResources):
+    if (300 - itemResourceRate) > 0:
+        decayRate = (300 - itemResourceRate) * itemImmunity
+    elif (300 - itemResourceRate) <= 0:
+        decayRate = 5
     while landmarkResources > 10000:
-        return landmarkResources -= decayRate
+        landmarkResources -= decayRate
         if (landmarkResources - decayRate) < 10000:
             diff = landmarkResources - 10000
             landmarkResources = landmarkResources - diff
-        time.sleep(60)
+        # put in a delay of 60 seconds
     while landmarkResources < 10000:
-        return landmarkResources += decayRate
+        landmarkResources += decayRate
         if (landmarkResources + decayRate) < 10000:
             diff = landmarkResources - 10000
             landmarkResources = landmarkResources + diff
-        time.sleep(60)
+        # put in a delay of 60 seconds
+    while landmarkResources == 10000:
+        landmarkResources
+
 
 # don't know how we would classify people putting resources in
-def landmarkCapture(landmarkResources):
+def landmarkCapture(landmarkResources, inputResources):
     if landmarkResources <= 0:
-        landmarkTeam = None
-        inputResources = "resources put in by player"
-        if inputResources = True:
-            return "landmark->team" = "player->team"
-            return landmarkResources = inputResources
-        time.sleep(120)
-        if "landmark->team" = None:
-            return landmarkResources = 1000
+        print(landmarkResources)
+        #landmarkTeam = None
+        if inputResources > 0:
+            #"landmark->team" = "player->team"
+            landmarkResources = inputResources
+            return landmarkResources
+        # delay 120 seconds before sets neutral 1000 resources
+        #if "landmark->team" = None:
+            #return landmarkResources = 1000
+
