324f8f9a728d4009c17ec6e21da7f256e0e3b0f0
Daniel Neenan <daniel@automise.co.uk>
2018-01-27 16:54:15 +0000

    Moved landmarks to seperate file

---
+++ landmarkFunctions.py
@@ -0,0 +1,47 @@
+import numpy as np
+import time
+
+# calculate the effect landmarks have on player
+landmarkMod = 0.5 * itemContagion
+landmarks = []
+def landmarkPressureInfluence(landmarkMod, effectiveMod, landmarks):
+    if landmarks in dist:
+        if "landmarks->team" == "players->team":
+            for landmark in landmarks:
+                return effectiveMod = effectiveMod + landmarkMod
+        if "landmarks->team" != "players->team"
+            for landmark in landmarks:
+                effectiveMod = effectiveMod - landmarkMod
+                if effectiveMod > -0.75:
+                    return effectiveMod
+                else:
+                    return effectiveMod = -0.75
+
+# landmark growth rate
+def landmarkResourceRate(itemResourceRate, itemImmuity):
+    landmarkResources = 10000
+    decayRate = (300 + itemResourceRate) * itemImmunity
+
+    while landmarkResources > 10000:
+        return landmarkResources -= decayRate
+        if (landmarkResources - decayRate) < 10000:
+            diff = landmarkResources - 10000
+            landmarkResources = landmarkResources - diff
+        time.sleep(60)
+    while landmarkResources < 10000:
+        return landmarkResources += decayRate
+        if (landmarkResources + decayRate) < 10000:
+            diff = landmarkResources - 10000
+            landmarkResources = landmarkResources + diff
+        time.sleep(60)
+
+# don't know how we would classify people putting resources in
+def landmarkCapture(landmarkResources):
+    if landmarkResources <= 0:
+        "landmark->team" = None
+        if "player puts in resources":
+            return "landmark->team" = "player->team"
+            return landmarkResources = "reasources player put in"
+        time.sleep(120)
+        if "landmark->team" = None:
+            return landmarkResources = 1000
